8a5ffddb498370d473f4296d0dcd423ffe65bcc4
aaguilera0 <alejandroeaguilera0@gmail.com>
2025-10-01 23:48:46 -0300

    feat: agregar MasterPage y páginas iniciales

---
+++ TPPromoWeb_Equipo4B/altaCliente.master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPPromoWeb_Equipo4B
+{
+    public partial class altaCliente : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
